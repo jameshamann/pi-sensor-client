dd7d3430496353b029d2c684a83f412e0b51c28d
James Hamann <jameshamann@me.com>
2018-07-16 12:47:31 +0100

    working on simple ui

--- src/screens/home.js
+++ src/screens/home.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
 import AppBar from 'material-ui/AppBar';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
 
 class Home extends Component {
@@ -13,7 +19,27 @@ class Home extends Component {
          iconClassNameRight="muidocs-icon-navigation-expand-more"
       />
       <RaisedButton label="Hello World!" />
-
+      <div>
+      <Card style={{maxWidth: 345,  flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <CardContent>
+          <Typography gutterBottom variant="headline" component="h2">
+            Lizard
+          </Typography>
+          <Typography component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small" color="primary">
+            Share
+          </Button>
+          <Button size="small" color="primary">
+            Learn More
+          </Button>
+        </CardActions>
+      </Card>
+      </div>
     </MuiThemeProvider>
 
     );
